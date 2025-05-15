716a6b794610c03f86cd4d2ccc4b72ad16e1f75f
Janluy leonardo <janluy.moreno@gmail.com>
2025-05-14 23:37:38 -0500

    funcionalidad inicial de experience

--- resources/views/cv/experience/index.blade.php
+++ resources/views/cv/experience/index.blade.php
@@ -1,14 +1,118 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Experience') }}
-        </h2>
-    </x-slot>
+  <x-slot name="header">
+    <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+      {{ __('Experience') }}
+    </h2>
+  </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-            </div>
+  <div class="py-12">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+      <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+        <div class="card">
+          <div class="card-header">add experience information</div>
+          <div class="card-body">
+            <form action="{{ route('about.store') }}" method="post" class="requires-validation"
+              enctype="multipart/form-data" novalidate>
+              @csrf
+              <div class="row">
+                <div class="col-md-6 mx-auto mb-1 mt-2 text-center">
+                  <input class="form-control form-control-sm mt-2 @error('position') is-invalid @enderror"
+                    type="text" name="position" placeholder="{{ __('position') }}" value="{{ old('position') }}"
+                    required>
+                  <div class="valid-feedback">Position field is valid!</div>
+                  <div class="invalid-feedback">Position field cannot be blank!</div>
+                  @error('position')
+                    <div class="text-danger">{{ $message }}</div>
+                  @enderror
+                </div>
+                <div class="col-md-6 mx-auto mb-1 mt-2 text-center">
+                  <input class="form-control form-control-sm mt-2 @error('company') is-invalid @enderror" type="text"
+                    name="company" placeholder="{{ __('company') }}" value="{{ old('company') }}" required>
+                  <div class="valid-feedback">Company field is valid!</div>
+                  <div class="invalid-feedback">Company field cannot be blank!</div>
+                  @error('company')
+                    <div class="text-danger">{{ $message }}</div>
+                  @enderror
+                </div>
+                <div
+                  class="mt-2 col-md-12 col-sm-12 col-lg-12 mx-auto d-flex justify-content-center align-items-center">
+                  <div class="form-floating">
+                    <textarea cols="200" class="form-control form-control-sm mt-2 @error('functions') is-invalid @enderror"
+                      name="functions" placeholder="Leave a comment here" id="floatingTextarea">{{ old('functions') }}</textarea>
+                    <label for="floatingTextarea">{{ __('Functions of charge') }}</label>
+                    @error('functions')
+                      <div class="text-danger">{{ $message }}</div>
+                    @enderror
+                  </div>
+                </div>
+
+                <div class="col-md-3 mx-auto mb-1 mt-2 text-center">
+                  <input class="form-control form-control-sm mt-2 @error('startDate') is-invalid @enderror"
+                    type="date" name="startDate" id="startDate" value="{{ old('startDate') }}" required>
+                  <span style="font-size: 0.75em;">Fecha de inicio</span>
+                  <div class="valid-feedback mv-up">You selected a fecha de inicio!</div>
+                  <div class="invalid-feedback mv-up">Please select a fecha de inicio!</div>
+                  @error('startDate')
+                    <div class="text-danger">{{ $message }}</div>
+                  @enderror
+                </div>
+                <div class="col-md-3 mt-2">
+                  <div class="form-check">
+                    <input class="form-check-input" type="checkbox" id="currentlyPosition" name="currentlyPosition">
+                    <label class="form-check-label" for="currentlyPosition">¿currently position?</label>
+                  </div>
+                </div>
+                <div class="col-md-3 mx-auto mb-1 mt-2 text-center">
+                  <input class="form-control form-control-sm mt-2 @error('endDate') is-invalid @enderror" type="date"
+                    name="endDate" id="endDate" value="{{ old('endDate') }}" required>
+                  <span style="font-size: 0.75em;">Fecha de finalizacion</span>
+                  <div class="valid-feedback mv-up">You selected a fecha de finalizacion!</div>
+                  <div class="invalid-feedback mv-up">Please select a fecha de finalizacion!</div>
+                  @error('endDate')
+                    <div class="text-danger">{{ $message }}</div>
+                  @enderror
+                </div>
+                <div class="col-md-12 col-sm-12 col-lg-12 mx-auto d-flex justify-content-center align-items-center">
+                  <div class="form-button mt-2 mb-2 p-2 mx-auto">
+                    <button id="submit" type="submit" class="btn btn-secondary">{{ __('Add info') }}</button>
+                  </div>
+                </div>
+            </form>
+            <hr>
+            {{-- <table class="table">
+              <thead>
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">{{ __('Full Name') }}</th>
+                  <th scope="col">{{ __('DocumentNumber') }}</th>
+                  <th scope="col">{{ __('Phone') }}</th>
+                  <th scope="col">{{ __('Actions') }}</th>
+                </tr>
+              </thead>
+              <tbody>
+                @foreach ($abouts as $about)
+                  <tr>
+                    <th scope="row">{{ $about->id }}</th>
+                    <td>{{ $about->name }}</td>
+                    <td>{{ $about->document }}</td>
+                    <td>{{ $about->phone }}</td>
+                    <td>
+                      <a href="{{ route('about.show', $about->id) }}" class="btn btn-warning">Show</a>
+                      <a href="{{ route('about.edit', $about->id) }}" class="btn btn-primary">Edit</a>
+                      <form action="{{ route('about.destroy', $about->id) }}" method="POST"
+                        style="display:inline;">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-danger">Delete</button>
+                      </form>
+                    </td>
+                  </tr>
+                @endforeach
+              </tbody>
+            </table> --}}
+          </div>
         </div>
+      </div>
     </div>
+  </div>
 </x-app-layout>
---
+++ resources/views/cv/experience/show.blade.php
@@ -0,0 +1,86 @@
+<x-app-layout>
+  <x-slot name="header">
+    <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+      {{ __('About') }}
+    </h2>
+  </x-slot>
+
+  <div class="py-12">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+      <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+        <div class="card">
+          <div class="card-header">
+            <div class="row">
+              <div class="col-md-3 ">Información personal</div>
+              <div class="col-md-3 "></div>
+              <div class="col-md-3 "></div>
+              <div class="col-md-3 text-end">
+                <a href="{{ url()->previous() }}">Volver</a>
+              </div>
+            </div>
+          </div>
+          <div class="card-body">
+            <div class="row">
+              <div class="col-md-2 text-center mx-auto">
+                <img src="{{ asset('storage/' . $about->Photo) }}" class="rounded mx-auto d-block" alt="About Photo">
+                <span style="font-size: 0.75em;">Foto</span>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md-12 mx-auto mt-2 text-center">
+                {{ $about->personalProfile }}<br>
+                <span style="font-size: 0.75em;">Perfil personal</span>
+              </div>
+              <div class="col-md-2 mx-auto mt-2 text-center">
+                {{ $about->dateOfBirth }}<br>
+                <span style="font-size: 0.75em;">Fecha de nacimiento</span>
+              </div>
+              <div class="col-md-3 mx-auto mt-2 text-center">
+                {{ $about->name }}<br>
+                <span style="font-size: 0.75em;">Nombre completo</span>
+              </div>
+              <div class="col-md-2 mx-auto mt-2 text-center">
+                {{ $about->document }}<br>
+                <span style="font-size: 0.75em;">N° documento</span>
+              </div>
+              <div class="col-md-2 mx-auto mt-2 text-center">
+                {{ $about->phone }}<br>
+                <span style="font-size: 0.75em;">N° Telefono</span>
+              </div>
+              <div class="col-md-2 mx-auto mt-2 text-center">
+                {{ $about->city }}<br>
+                <span style="font-size: 0.75em;">Ciudad</span>
+              </div>
+              <div class="col-md-2 mx-auto mt-2 text-center">
+                {{ $about->department }}<br>
+                <span style="font-size: 0.75em;">Departamento</span>
+              </div>
+              <div class="col-md-2 mx-auto mt-2 text-center">
+                {{ $about->locality }}<br>
+                <span style="font-size: 0.75em;">Departamento</span>
+              </div>
+              <div class="col-md-2 mx-auto mt-2 text-center">
+                {{ $about->address }}<br>
+                <span style="font-size: 0.75em;">Direccion</span>
+              </div>
+              <div class="col-md-2 mx-auto mt-2 text-center">
+                {{ $about->barrio }}<br>
+                <span style="font-size: 0.75em;">Barrio</span>
+              </div>
+              <!-- Sección para redes sociales -->
+              @if (!empty($about->social_media_links))
+                <h4>Redes Sociales</h4>
+                @foreach ($about->social_media_links as $platform => $link)
+                  <div class="col-md-3 mx-auto mt-2 text-center">
+                    {{ $link }}<br>
+                    <span style="font-size: 0.75em;">{{ $platform }}</span>
+                  </div>
+                @endforeach
+              @endif
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</x-app-layout>
