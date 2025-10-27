ff3debc02cc115d2814f2e52b4e5715f5e7dda0b
janluyleonardo <janluy.moreno@gmail.com>
2025-10-26 20:47:23 -0500

    suprimi campos de validacion que se estaba repitiendo

--- resources/views/cv/About/index.blade.php
+++ resources/views/cv/About/index.blade.php
@@ -52,8 +52,6 @@
                     <input class="form-control form-control-sm mt-2 @error('dateOfBirth') is-invalid @enderror"
                       type="date" name="dateOfBirth" id="fechaNacimiento" value="{{ old('dateOfBirth') }}" required>
                     <span style="font-size: 0.75em;">Fecha de nacimiento</span>
-                    <div class="valid-feedback mv-up">You selected a fecha de nacimiento!</div>
-                    <div class="invalid-feedback mv-up">Please select a fecha de nacimiento!</div>
                     @error('dateOfBirth')
                       <div class="text-danger">{{ $message }}</div>
                     @enderror
@@ -62,8 +60,6 @@
                     <input class="form-control form-control-sm mt-2 @error('Photo') is-invalid @enderror" type="file"
                       name="Photo" accept="image/png, image/jpeg" required>
                     <span style="font-size: 0.75em;">Foto 3X4</span>
-                    <div class="valid-feedback">UserPhoto field is valid!</div>
-                    <div class="invalid-feedback">UserPhoto field cannot be blank!</div>
                     @error('Photo')
                       <div class="text-danger">{{ $message }}</div>
                     @enderror
@@ -71,8 +67,6 @@
                   <div class="col-md-6 mx-auto mb-1 mt-2 text-center">
                     <input class="form-control form-control-sm mt-2 @error('name') is-invalid @enderror" type="text"
                       name="name" placeholder="{{ __('Full Name') }}" value="{{ old('name') }}" required>
-                    <div class="valid-feedback">Username field is valid!</div>
-                    <div class="invalid-feedback">Username field cannot be blank!</div>
                     @error('name')
                       <div class="text-danger">{{ $message }}</div>
                     @enderror
@@ -80,8 +74,6 @@
                   <div class="col-md-3 mx-auto mt-2">
                     <input class="form-control form-control-sm mt-2 @error('document') is-invalid @enderror"
                       type="number" name="document" placeholder="N° documento" value="{{ old('document') }}" required>
-                    <div class="valid-feedback">Numero de documento field is valid!</div>
-                    <div class="invalid-feedback">Numero de documento field cannot be blank!</div>
                     @error('document')
                       <div class="text-danger">{{ $message }}</div>
                     @enderror
@@ -89,8 +81,6 @@
                   <div class="col-md-3 mx-auto mt-2">
                     <input class="form-control form-control-sm mt-2 @error('phone') is-invalid @enderror" type="number"
                       name="phone" placeholder="N° telefono" value="{{ old('phone') }}" required>
-                    <div class="valid-feedback">Telefono field is valid!</div>
-                    <div class="invalid-feedback">Telefono field cannot be blank!</div>
                     @error('phone')
                       <div class="text-danger">{{ $message }}</div>
                     @enderror
@@ -98,8 +88,6 @@
                   <div class="col-md-4 mx-auto mt-2">
                     <input class="form-control form-control-sm mt-2 @error('city') is-invalid @enderror" type="text"
                       name="city" placeholder="city" value="{{ old('city') }}" required>
-                    <div class="valid-feedback">city field is valid!</div>
-                    <div class="invalid-feedback">city field cannot be blank!</div>
                     @error('city')
                       <div class="text-danger">{{ $message }}</div>
                     @enderror
@@ -108,8 +96,6 @@
                     <input class="form-control form-control-sm mt-2 @error('department') is-invalid @enderror"
                       type="text" name="department" placeholder="department" value="{{ old('department') }}"
                       required>
-                    <div class="valid-feedback">department field is valid!</div>
-                    <div class="invalid-feedback">department field cannot be blank!</div>
                     @error('department')
                       <div class="text-danger">{{ $message }}</div>
                     @enderror
@@ -124,86 +110,77 @@
                         </option>
                       @endforeach
                     </select>
-                    <div class="valid-feedback mv-up">You selected a locality!</div>
-                    <div class="invalid-feedback mv-up">Please select a locality!</div>
-                    @error('locality')
+                    @error('id_locality')
                       <div class="text-danger">{{ $message }}</div>
                     @enderror
                   </div>
                   <div class="col-md-6 mx-auto mb-1 mt-2 text-center">
                     <input class="form-control form-control-sm mt-2 @error('address') is-invalid @enderror"
                       type="text" name="address" placeholder="address" value="{{ old('address') }}" required>
-                    <div class="valid-feedback">address field is valid!</div>
-                    <div class="invalid-feedback">address field cannot be blank!</div>
                     @error('address')
                       <div class="text-danger">{{ $message }}</div>
                     @enderror
                   </div>
-                  <div class="col-md-6 mx-auto mt-2">
+                  <div class="col-md-6 mx-auto mt-2 text-center">
                     <input class="form-control form-control-sm mt-2 @error('barrio') is-invalid @enderror"
                       type="text" name="barrio" placeholder="Barrio" value="{{ old('barrio') }}" required>
-                    <div class="valid-feedback">Barrio field is valid!</div>
-                    <div class="invalid-feedback">Barrio field cannot be blank!</div>
                     @error('barrio')
                       <div class="text-danger">{{ $message }}</div>
                     @enderror
                   </div>
-                  <div
-                    class="mt-2 col-md-12 col-sm-12 col-lg-12 mx-auto d-flex justify-content-center align-items-center">
-                    <div class="form-floating">
-                      <textarea cols="200" class="form-control form-control-sm mt-2 @error('personalProfile') is-invalid @enderror"
-                        name="personalProfile" placeholder="Leave a comment here" id="floatingTextarea">{{ old('personalProfile') }}</textarea>
-                      <label for="floatingTextarea">{{ __('Personal profile') }}</label>
-                      @error('personalProfile')
-                        <div class="text-danger">{{ $message }}</div>
-                      @enderror
-                    </div>
-                  </div>
-                  <!-- Sección para redes sociales -->
-                  <div class="col-md-3 mt-2">
-                    <div class="form-check">
-                      <input class="form-check-input" type="checkbox" id="facebook" name="social_media[facebook]">
-                      <label class="form-check-label" for="facebook">Facebook</label>
-                      <input type="text" id="facebook_link" name="social_media_links[facebook]"
-                        class="form-control mt-2" style="display: none;" placeholder="Enlace de Facebook">
-                    </div>
-                  </div>
-                  <div class="col-md-3 mt-2">
-                    <div class="form-check">
-                      <input class="form-check-input" type="checkbox" id="twitter" name="social_media[twitter]">
-                      <label class="form-check-label" for="twitter">Twitter</label>
-                      <input type="text" id="twitter_link" name="social_media_links[twitter]"
-                        class="form-control mt-2" style="display: none;" placeholder="Enlace de Twitter">
-                    </div>
-                  </div>
-                  <div class="col-md-3 mt-2">
-                    <div class="form-check">
-                      <input class="form-check-input" type="checkbox" id="instagram" name="social_media[instagram]">
-                      <label class="form-check-label" for="instagram">Instagram</label>
-                      <input type="text" id="instagram_link" name="social_media_links[instagram]"
-                        class="form-control mt-2" style="display: none;" placeholder="Enlace de Instagram">
-                    </div>
-                  </div>
-                  <div class="col-md-3 mt-2">
-                    <div class="form-check">
-                      <input class="form-check-input" type="checkbox" id="linkedin" name="social_media[linkedin]">
-                      <label class="form-check-label" for="linkedin">LinkedIn</label>
-                      <input type="text" id="linkedin_link" name="social_media_links[linkedin]"
-                        class="form-control mt-2" style="display: none;" placeholder="Enlace de LinkedIn">
-                    </div>
-                  </div>
-                  <div class="col-md-12 col-sm-12 col-lg-12 mx-auto d-flex justify-content-center align-items-center">
-                    <div class="form-button mt-2 mb-2 p-2 mx-auto">
-                      <button id="submit" type="submit" class="btn btn-secondary">{{ __('Add info') }}</button>
-                    </div>
+                  <div class="mt-2 col-md-12 mx-auto text-center">
+                    <textarea cols="200" class="form-control form-control-sm mt-2 @error('personalProfile') is-invalid @enderror"
+                      name="personalProfile" placeholder="Perfil profesional" id="floatingTextarea">{{ old('personalProfile') }}</textarea>
+                    @error('personalProfile')
+                      <div class="text-danger">{{ $message }}</div>
+                    @enderror
                   </div>
                 </div>
-              </form>
-            @endif
+                <!-- Sección para redes sociales -->
+                <div class="col-md-3 mt-2 bg-slate-500">
+                  <div class="form-check">
+                    <input class="form-check-input" type="checkbox" id="facebook" name="social_media[facebook]">
+                    <label class="form-check-label" for="facebook">Facebook</label>
+                    <input type="text" id="facebook_link" name="social_media_links[facebook]"
+                      class="form-control mt-2" style="display: none;" placeholder="Enlace de Facebook">
+                  </div>
+                </div>
+                <div class="col-md-3 mt-2">
+                  <div class="form-check">
+                    <input class="form-check-input" type="checkbox" id="twitter" name="social_media[twitter]">
+                    <label class="form-check-label" for="twitter">Twitter</label>
+                    <input type="text" id="twitter_link" name="social_media_links[twitter]"
+                      class="form-control mt-2" style="display: none;" placeholder="Enlace de Twitter">
+                  </div>
+                </div>
+                <div class="col-md-3 mt-2">
+                  <div class="form-check">
+                    <input class="form-check-input" type="checkbox" id="instagram" name="social_media[instagram]">
+                    <label class="form-check-label" for="instagram">Instagram</label>
+                    <input type="text" id="instagram_link" name="social_media_links[instagram]"
+                      class="form-control mt-2" style="display: none;" placeholder="Enlace de Instagram">
+                  </div>
+                </div>
+                <div class="col-md-3 mt-2">
+                  <div class="form-check">
+                    <input class="form-check-input" type="checkbox" id="linkedin" name="social_media[linkedin]">
+                    <label class="form-check-label" for="linkedin">LinkedIn</label>
+                    <input type="text" id="linkedin_link" name="social_media_links[linkedin]"
+                      class="form-control mt-2" style="display: none;" placeholder="Enlace de LinkedIn">
+                  </div>
+                </div>
+                <div class="col-md-12 col-sm-12 col-lg-12 mx-auto d-flex justify-content-center align-items-center">
+                  <div class="form-button mt-2 mb-2 p-2 mx-auto">
+                    <button id="submit" type="submit" class="btn btn-secondary">{{ __('Add info') }}</button>
+                  </div>
+                </div>
           </div>
+          </form>
+          @endif
         </div>
       </div>
     </div>
+  </div>
   </div>
 </x-app-layout>
 
